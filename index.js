dc18708ed2c5058610566f59f3ddf0c9e43f0132
Utlamo <danielkasiurak1@gmail.com>
2023-02-27 00:55:03 +0100

    Commit start bot

---
+++ index.js
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Channel],
+});
+
+client.on('messageCreate', async (message) => {
+  client.user.setActivity(message.content);
+});
+
+
+//
+
+
+//make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN);
