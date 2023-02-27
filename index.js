27ff3e410d666090f3fb60ec8c2ba763d7832779
Utlamo <danielkasiurak1@gmail.com>
2023-02-27 02:01:03 +0100

    remove donenv

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
 import { Client, GatewayIntentBits, Partials } from "discord.js";
 
 
